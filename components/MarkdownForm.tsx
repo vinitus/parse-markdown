69fba1403c84f6aa1056dfd7d3c4c5ad69ed4a13
vinitus <tlsdnrng@gmail.com>
2023-08-17 00:31:35 +0900

    마크다운 파일을 string으로 불러와서 textarea에 넣기

--- components/MarkdownForm.tsx
+++ components/MarkdownForm.tsx
@@ -1,14 +1,35 @@
-import loadMarkdown from '@/utils/loadMarkdown';
+'use client';
 
-export default async function MarkdownForm({ filename }: { filename: string }) {
-  const markdown = await loadMarkdown(filename);
+import loadMarkdown from '@/utils/loadMarkdown';
+import { use, useState, useEffect, ChangeEvent } from 'react';
+
+export default function MarkdownForm({ filename }: { filename: string }) {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    async function a() {
+      const markdownFile = await loadMarkdown(filename);
+      setMarkdown(markdownFile);
+    }
+    a();
+  }, [filename]);
+
+  function markdownChangeHandler(event: ChangeEvent<HTMLTextAreaElement>) {
+    setMarkdown(event.target.value);
+  }
 
   return (
     <>
-      <h1>{filename + '.md'}</h1>
-      <form action=''>
-        <div contentEditable='true' id='' dangerouslySetInnerHTML={{ __html: markdown }}></div>
-      </form>
+      {markdown ? (
+        <>
+          <h1>{filename + '.md'}</h1>
+          <form action='' className='w-full h-full'>
+            <textarea name='markdown-editor' id='markdown-editor' defaultValue={markdown} onChange={markdownChangeHandler} className='w-full h-full' />
+          </form>
+        </>
+      ) : (
+        <div>loading...</div>
+      )}
     </>
   );
 }
