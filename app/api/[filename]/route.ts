f8796c0809f6c93fe7528404bd18e8a2d71632e7
vinitus <tlsdnrng@gmail.com>
2023-08-16 20:22:57 +0900

    동적 라우팅을 위해 파일이동 + 파일 읽기 기능

---
+++ app/api/[filename]/route.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, { params }: { params: { filename: string } }) {
+  const { filename } = params;
+  const file = fs.readFileSync(`C://Users/vinitus/my-blog/public/${filename}.md`, 'utf8');
+
+  return NextResponse.json({ file });
+}
