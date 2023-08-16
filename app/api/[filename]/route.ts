21be1f05a448f834bc0efb70f293faf060863cdb
vinitus <tlsdnrng@gmail.com>
2023-08-16 20:41:19 +0900

    transformSTringToMarkdownURL 함수 적용

--- app/api/[filename]/route.ts
+++ app/api/[filename]/route.ts
@@ -1,9 +1,11 @@
+import transformStringToMarkdownURL from '@/utils/transformStringToMarkdownURL';
 import fs from 'fs';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: { params: { filename: string } }) {
   const { filename } = params;
-  const file = fs.readFileSync(`C://Users/vinitus/my-blog/public/${filename}.md`, 'utf8');
+  const url = transformStringToMarkdownURL(filename);
+  const file = fs.readFileSync(url, 'utf8');
 
   return NextResponse.json({ markdown: file });
 }
