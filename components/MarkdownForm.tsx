c1b8c09924b987c04d584157b75cec5f68e4688c
vinitus <tlsdnrng@gmail.com>
2023-08-16 20:42:33 +0900

    fetch 경로를 api 경로로 수정, textarea를 div로 대체

--- components/MarkdownForm.tsx
+++ components/MarkdownForm.tsx
@@ -1,15 +1,13 @@
 import loadMarkdown from '@/utils/loadMarkdown';
-import transformStringToMarkdownURL from '@/utils/transformStringToMarkdownURL';
 
 export default async function MarkdownForm({ filename }: { filename: string }) {
-  const requestURL = transformStringToMarkdownURL(filename);
-  const markdown = loadMarkdown(requestURL);
+  const markdown = await loadMarkdown(filename);
 
   return (
     <>
       <h1>{filename + '.md'}</h1>
       <form action=''>
-        <textarea name='' id='' cols={30} rows={10} dangerouslySetInnerHTML={{ __html: markdown }}></textarea>
+        <div contentEditable='true' id='' dangerouslySetInnerHTML={{ __html: markdown }}></div>
       </form>
     </>
   );
