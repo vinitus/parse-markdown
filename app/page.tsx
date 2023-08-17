e890eb7184592ba39d217dac7b36bda5cbb34bdf
vinitus <tlsdnrng@gmail.com>
2023-08-17 20:24:50 +0900

    header 컴포넌트를 분리하고 root layout에 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,24 @@
 import { ReactNode } from 'react';
-import Image from 'next/image';
-import blogLogo from '@/public/blog-logo.png';
-import navbarCss from '@/app/navbar.module.css';
 import MarkdownForm from '@/components/MarkdownForm';
-import Link from 'next/link';
 
 export default function Home({ children }: { children: ReactNode }) {
   console.log(children);
   return (
-    <>
-      <header className={navbarCss.navbarHeader}>
-        <nav className={navbarCss.navbarLayout}>
-          <div className={navbarCss.navbarCategoryArea}>
-            <Image src={blogLogo} alt='블로그 로고' />
-            <p>Home</p>
-            <p>Post</p>
-            <Link href={'/markdown'}>
-              <p>Mardown</p>
-            </Link>
-            <p>TEMPT</p>
-            <p>TEMPT</p>
-          </div>
-          {/* search */}
-          <div className={navbarCss.navbarSearchArea}>
-            <button className={navbarCss.navbarSearchBox}>
-              검색
-              <p className={navbarCss.navbarSearchBoxButton}>검색</p>
-            </button>
-            <a href='' className={navbarCss.navbarContactButton}>
-              <p className={navbarCss.navbarContactButtonFont}>Contact</p>
-            </a>
-          </div>
-        </nav>
-      </header>
-      <main>
-        <div className='max-w-screen-xl mx-auto p-10 flex flex-row w-full'>
-          {/* sidebar */}
-          <div className='sticky h-[100vh] flex-shrink-0 flex-col justify-between w-72'>
-            <ul>1</ul>
-            <ul>2</ul>
-            <ul>3</ul>
-            <ul>4</ul>
-            <ul>5</ul>
-          </div>
-          <article className='w-full'>
-            <MarkdownForm filename='HTTP,TCPIP' />
-          </article>
+    <main>
+      <div className='max-w-screen-xl mx-auto p-10 flex flex-row w-full'>
+        {/* sidebar */}
+        <div className='sticky h-[100vh] flex-shrink-0 flex-col justify-between w-72'>
+          <ul>1</ul>
+          <ul>2</ul>
+          <ul>3</ul>
+          <ul>4</ul>
+          <ul>5</ul>
         </div>
-        {children}
-      </main>
-    </>
+        <article className='w-full'>
+          <MarkdownForm filename='HTTP,TCPIP' />
+        </article>
+      </div>
+      {children}
+    </main>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import blogLogo from '@/public/blog-logo.png';
+import navbarCss from '@/app/navbar.module.css';
+
+export default function Header() {
+  return (
+    <header className={navbarCss.navbarHeader}>
+      <nav className={navbarCss.navbarLayout}>
+        <div className={navbarCss.navbarCategoryArea}>
+          <Image src={blogLogo} alt='블로그 로고' />
+          <p>Home</p>
+          <p>Post</p>
+          <Link href={'/markdown'}>
+            <p>Mardown</p>
+          </Link>
+          <p>TEMPT</p>
+          <p>TEMPT</p>
+        </div>
+        {/* search */}
+        <div className={navbarCss.navbarSearchArea}>
+          <button className={navbarCss.navbarSearchBox}>
+            검색
+            <p className={navbarCss.navbarSearchBoxButton}>검색</p>
+          </button>
+          <a href='' className={navbarCss.navbarContactButton}>
+            <p className={navbarCss.navbarContactButtonFont}>Contact</p>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
