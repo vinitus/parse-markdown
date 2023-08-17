af75484a59d0ccdbf8040a222da50e9105f73b0a
vinitus <tlsdnrng@gmail.com>
2023-08-17 20:27:59 +0900

    sidebar와 main을 layout으로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import { ReactNode } from 'react';
 import MarkdownForm from '@/components/MarkdownForm';
 
-export default function Home({ children }: { children: ReactNode }) {
-  console.log(children);
+export default function Home() {
   return (
-    <main>
-      <div className='max-w-screen-xl mx-auto p-10 flex flex-row w-full'>
-        {/* sidebar */}
-        <div className='sticky h-[100vh] flex-shrink-0 flex-col justify-between w-72'>
-          <ul>1</ul>
-          <ul>2</ul>
-          <ul>3</ul>
-          <ul>4</ul>
-          <ul>5</ul>
-        </div>
-        <article className='w-full'>
-          <MarkdownForm filename='HTTP,TCPIP' />
-        </article>
-      </div>
-      {children}
-    </main>
+    <article className='w-full'>
+      <MarkdownForm filename='HTTP,TCPIP' />
+    </article>
   );
 }
