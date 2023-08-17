c77a7c4f2133a55989b8ac5d96ad5bfefe7f31ec
vinitus <tlsdnrng@gmail.com>
2023-08-17 21:13:02 +0900

    URI 인코딩 문제 해결

--- app/markdown/[filename]/page.tsx
+++ app/markdown/[filename]/page.tsx
@@ -2,6 +2,7 @@ import MarkdownForm from '@/components/MarkdownForm';
 
 export default function MarkdownEditor({ params }: { params: { filename: string } }) {
   const { filename } = params;
+  const decodedFilename = decodeURIComponent(filename);
 
-  return <MarkdownForm filename={filename} />;
+  return <MarkdownForm filename={decodedFilename} />;
 }
