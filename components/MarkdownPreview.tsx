749b18842cd3ba0cd7743cb21f9cbcd2fd1a63eb
vinitus <tlsdnrng@gmail.com>
2023-08-17 21:58:34 +0900

    markdown-tojsx 모듈을 사용한 마크다운 파싱 및 미리보기 컴포넌트

---
+++ components/MarkdownPreview.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Markdown from 'markdown-to-jsx';
+
+export default function MarkdownRenderer({ markdownContent }: { markdownContent: string }) {
+  return (
+    <div className='markdown-renderer'>
+      <Markdown options={{ wrapper: 'article' }}>{markdownContent}</Markdown>
+    </div>
+  );
+}
