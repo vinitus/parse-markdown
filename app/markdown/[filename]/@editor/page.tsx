d3f98e60592ddd8f3111551124d0a6856400b07a
vinitus <tlsdnrng@gmail.com>
2023-08-18 21:34:48 +0900

    병렬 라우팅 제거

--- app/markdown/[filename]/@editor/page.tsx
+++
@@ -1,8 +0,0 @@
-import MarkdownForm from '@/components/MarkdownForm';
-
-export default function MarkdownEditor({ params }: { params: { filename: string } }) {
-  const { filename } = params;
-  const decodedFilename = decodeURIComponent(filename);
-
-  return <MarkdownForm filename={decodedFilename} />;
-}
