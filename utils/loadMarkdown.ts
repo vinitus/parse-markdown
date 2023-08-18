d6aa5221c5ad35a89d42258d71890d3288166c6e
vinitus <tlsdnrng@gmail.com>
2023-08-18 21:31:25 +0900

    loading.js 테스트를 위한 setTimeout

--- utils/loadMarkdown.ts
+++ utils/loadMarkdown.ts
@@ -1,5 +1,9 @@
 export default async function loadMarkdown(filename: string) {
-  const res = await fetch(`http://localhost:3000/api/${filename}`);
+  const res = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(fetch(`http://localhost:3000/api/${filename}`));
+    }, 1000);
+  });
   const { markdown } = await res.json();
 
   return markdown;
