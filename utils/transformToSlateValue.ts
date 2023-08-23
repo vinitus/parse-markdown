d6b5e6d3703e76ab15793531006cf74862f0e0cd
vinitus <tlsdnrng@gmail.com>
2023-08-23 16:25:59 +0900

    \r이 필요없다고 생각이 듬

--- utils/transformToSlateValue.ts
+++ utils/transformToSlateValue.ts
@@ -3,35 +3,34 @@ import { isElement, isText } from './typeguard';
 
 export default function transformToSlateValue(str: string): Descendant[] {
   const splitedStr = str.split('\n');
+  console.log(splitedStr.length);
   const descendantArray: Descendant[] = splitedStr.map((line) => {
     return {
-      children: [{ text: line }],
+      children: [{ text: line.trim() }],
     };
   });
 
-  const replaceBlankToSpaceArray = replaceBlankToSpace(descendantArray);
-
-  return replaceBlankToSpaceArray;
+  return descendantArray;
 }
 
-function replaceBlankToSpace(descendantArray: Descendant[]): Descendant[] {
-  const copiedArray = descendantArray.map((descendant) => {
-    if (isElement(descendant)) {
-      if (isText(descendant.children[0])) {
-        if (descendant.children[0].text === '\r') {
-          const a: Element = {
-            children: [
-              {
-                text: '   \r',
-              },
-            ],
-          };
-          return a;
-        }
-      }
-    }
-    return descendant;
-  });
+// function replaceBlankToSpace(descendantArray: Descendant[]): Descendant[] {
+//   const copiedArray = descendantArray.map((descendant) => {
+//     if (isElement(descendant)) {
+//       if (isText(descendant.children[0])) {
+//         if (descendant.children[0].text === '\r') {
+//           const a: Element = {
+//             children: [
+//               {
+//                 text: '   \r',
+//               },
+//             ],
+//           };
+//           return a;
+//         }
+//       }
+//     }
+//     return descendant;
+//   });
 
-  return copiedArray;
-}
+//   return copiedArray;
+// }
