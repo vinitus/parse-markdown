624da8662e13df71a1f49c39d496c5bf26aabe54
vinitus <tlsdnrng@gmail.com>
2023-08-23 21:51:48 +0900

    slate 들어내기

--- utils/typeguard.ts
+++
@@ -1,9 +0,0 @@
-import { Descendant, Element, Text } from 'slate';
-
-export function isElement(descendant: Descendant): descendant is Element {
-  return (descendant as Element) ? true : false;
-}
-
-export function isText(descendant: Descendant): descendant is Text {
-  return (descendant as Text) ? true : false;
-}
