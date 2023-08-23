c39b34afb1acb8f476434af001768c463d580937
vinitus <tlsdnrng@gmail.com>
2023-08-23 21:05:10 +0900

    wrapper option 제거

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -4,5 +4,5 @@ import React from 'react';
 import Markdown from 'markdown-to-jsx';
 
 export default function MarkdownRenderer({ markdownContent }: { markdownContent: string }) {
-  return <Markdown options={{ wrapper: 'article' }}>{markdownContent}</Markdown>;
+  return <Markdown>{markdownContent}</Markdown>;
 }
