2d08fac32d11351523e0551b43ebe26c7a8a5319
vinitus <tlsdnrng@gmail.com>
2023-08-23 20:53:00 +0900

    마크다운 프리뷰 공백 구현

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -7,11 +7,16 @@ import MarkdownPreview from './MarkdownPreview';
 export default function Markdown({ markdownDataObj }: { markdownDataObj: { markdownContent: string; filename: string } }) {
   const { markdownContent, filename } = markdownDataObj;
   const [markdown, setMarkdown] = useState(markdownContent);
+  const splitedMarkdown = markdown.split('\n');
 
   return (
     <div className='flex flex-row'>
       <MarkdownForm filename={filename} markdown={markdown} setMarkdown={setMarkdown} />
-      <MarkdownPreview markdownContent={markdown} />
+      <div className='w-[50%] flex flex-col'>
+        {splitedMarkdown.map((line, idx) => {
+          return line === '\r' ? <div className='h-6' key={idx} /> : <MarkdownPreview markdownContent={line} key={idx} />;
+        })}
+      </div>
     </div>
   );
 }
--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -4,9 +4,5 @@ import React from 'react';
 import Markdown from 'markdown-to-jsx';
 
 export default function MarkdownRenderer({ markdownContent }: { markdownContent: string }) {
-  return (
-    <div className='w-[50%]'>
-      <Markdown options={{ wrapper: 'article' }}>{markdownContent}</Markdown>
-    </div>
-  );
+  return <Markdown options={{ wrapper: 'article' }}>{markdownContent}</Markdown>;
 }
