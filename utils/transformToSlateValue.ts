29c1ab4b19f5e9ab42ec5a8a2005842d4c6ca392
vinitus <tlsdnrng@gmail.com>
2023-08-23 15:16:37 +0900

    읽은 마크다운 파일을 개행문자를 기준으로 하여 slate의 Descendant[] 타입으로 만들기

--- utils/transformToSlateValue.ts
+++ utils/transformToSlateValue.ts
@@ -1,13 +1,44 @@
-import { Descendant } from 'slate';
+import { BaseElement, Descendant, Element, Text } from 'slate';
 
 export default function transformToSlateValue(str: string): Descendant[] {
   const splitedStr = str.split('\n');
-  const descendantArray = splitedStr.map((line) => {
+  const descendantArray: Descendant[] = splitedStr.map((line) => {
     return {
-      type: 'paragraph',
       children: [{ text: line }],
     };
   });
 
-  return descendantArray;
+  const replaceBlankToSpaceArray = replaceBlankToSpace(descendantArray);
+
+  return replaceBlankToSpaceArray;
+}
+
+function replaceBlankToSpace(descendantArray: Descendant[]): Descendant[] {
+  const copiedArray = descendantArray.map((descendant) => {
+    if (isElement(descendant)) {
+      if (isText(descendant.children[0])) {
+        if (descendant.children[0].text === '\r') {
+          const a: Element = {
+            children: [
+              {
+                text: '   \r',
+              },
+            ],
+          };
+          return a;
+        }
+      }
+    }
+    return descendant;
+  });
+
+  return copiedArray;
+}
+
+function isElement(descendant: Descendant): descendant is Element {
+  return (descendant as Element) ? true : false;
+}
+
+function isText(descendant: Descendant): descendant is Text {
+  return (descendant as Text) ? true : false;
 }
