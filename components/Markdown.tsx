b1b226b129e4fe95cb497d61d00fd0751a76518d
vinitus <tlsdnrng@gmail.com>
2023-08-24 13:29:36 +0900

    파일 제목에 대한 스타일링 추가

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import MarkdownForm from './MarkdownForm';
 import MarkdownPreview from './MarkdownPreview';
 
@@ -8,10 +8,19 @@ export default function Markdown({ markdownDataObj }: { markdownDataObj: { markd
   const { markdownContent, filename } = markdownDataObj;
   const [markdown, setMarkdown] = useState(markdownContent);
 
+  const parsedFilename = useMemo(() => decodeURIComponent(filename) + '.md', [filename]);
+
   return (
-    <div className='flex flex-row'>
-      <MarkdownForm filename={filename} markdown={markdown} setMarkdown={setMarkdown} />
-      <MarkdownPreview markdown={markdown} />
+    <div className='flex flex-col'>
+      <span className='h-12 px-4'>
+        <p className='min-h-8 px-2 py-1 font-bold border border-[#7a828e] rounded-md outline-none inline-flex items-stretch text-[#7a828e] bg-[#9ea7b3] bg-opacity-0'>
+          {parsedFilename}
+        </p>
+      </span>
+      <div className='flex flex-row'>
+        <MarkdownForm markdown={markdown} setMarkdown={setMarkdown} />
+        <MarkdownPreview markdown={markdown} />
+      </div>
     </div>
   );
 }
