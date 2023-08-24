5c6fb8bbc03be25b0bc768a1cdc944e4c2d43416
vinitus <tlsdnrng@gmail.com>
2023-08-24 12:44:18 +0900

    markdown filename을 Markdown 컴포넌트로 옮기기

--- components/MarkdownForm.tsx
+++ components/MarkdownForm.tsx
@@ -3,19 +3,17 @@
 import { ChangeEvent } from 'react';
 
 interface Props {
-  filename: string;
   markdown: string;
   setMarkdown: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function MarkdownForm({ filename, markdown, setMarkdown }: Props) {
+export default function MarkdownForm({ markdown, setMarkdown }: Props) {
   function markdownChangeHandler(event: ChangeEvent<HTMLTextAreaElement>) {
     setMarkdown(event.target.value);
   }
 
   return (
     <div className='w-[50%] h-[calc(100vh-195px)]'>
-      <h1>{filename + '.md'}</h1>
       <form action='' className='h-full'>
         <textarea name='markdown-editor' id='markdown-editor' defaultValue={markdown} onChange={markdownChangeHandler} className='w-full h-full' />
       </form>
