ce16b9ccf43d6bd18b98ab1bdf72a79e94f298f1
vinitus <tlsdnrng@gmail.com>
2023-08-24 11:52:01 +0900

    react-markdown 적용하기

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,23 +1,19 @@
 'use client';
 
 import { useState } from 'react';
+import remarkGfm from 'remark-gfm';
+import ReactMarkdown from 'react-markdown';
 import MarkdownForm from './MarkdownForm';
-import MarkdownPreview from './MarkdownPreview';
-import markdownParser from '@/utils/markdownParser';
 
 export default function Markdown({ markdownDataObj }: { markdownDataObj: { markdownContent: string; filename: string } }) {
   const { markdownContent, filename } = markdownDataObj;
   const [markdown, setMarkdown] = useState(markdownContent);
-  const splitedMarkdown = markdown.split('\n');
-  markdownParser(markdownContent);
 
   return (
     <div className='flex flex-row'>
       <MarkdownForm filename={filename} markdown={markdown} setMarkdown={setMarkdown} />
-      <div className='w-[50%] flex flex-col'>
-        {splitedMarkdown.map((line, idx) => {
-          return line === '\r' || !line ? <span className='h-6' key={idx} /> : <MarkdownPreview markdownContent={line} key={idx} />;
-        })}
+      <div className='flex flex-col w-[50%]'>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
       </div>
     </div>
   );
