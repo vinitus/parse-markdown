10238b96002be4118fc2dfa6448a17080c81cc76
vinitus <tlsdnrng@gmail.com>
2023-08-24 12:32:23 +0900

    폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,39 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Sidebar from '@/components/Sidebar';
 
+import localFont from 'next/font/local';
+
 const inter = Inter({ subsets: ['latin'] });
+
+const myFont = localFont({
+  src: [
+    {
+      path: './font/SpoqaHanSansNeo-Bold.ttf',
+      weight: '700',
+      style: 'bold',
+    },
+    {
+      path: './font/SpoqaHanSansNeo-Light.ttf',
+      weight: '400',
+      style: 'light',
+    },
+    {
+      path: './font/SpoqaHanSansNeo-Medium.ttf',
+      weight: '600',
+      style: 'medium',
+    },
+    {
+      path: './font/SpoqaHanSansNeo-Regular.ttf',
+      weight: '500',
+      style: 'regular',
+    },
+    {
+      path: './font/SpoqaHanSansNeo-Thin.ttf',
+      weight: '300',
+      style: 'thin',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +46,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='kr'>
-      <body className={inter.className}>
+      <body className={myFont.className}>
         <Header />
         <main>
           <div className='max-w-screen-xl mx-auto p-10 flex flex-row w-full'>
