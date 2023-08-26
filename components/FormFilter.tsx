f7ea0e68f50697db122e233c15732fe88d46a175
vinitus <tlsdnrng@gmail.com>
2023-08-26 20:09:06 +0900

    억지로 재사용하는 것 같아서 원래대로

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -1,11 +1,22 @@
-'use client';
+export default function FormFilter() {
+  return (
+    <div className='bg-[#272b33] p-2 text-base '>
+      <Input />
+      <Button>test</Button>
+      <br />
+      <Input />
+    </div>
+  );
+}
 
-import { useState, Dispatch, SetStateAction } from 'react';
-import { FilterTarget } from '@/utils/backtickAlgorithm';
+function Input() {
+  return <input type='text' className='border border-[#7a828e] rounded-md py-1 px-2 bg-[#0a0c10] text-[#f0f3f6] text-xs mr-2 my-1' />;
+}
 
-export default function FormFilter({ filterTarget, setFilterTarget }: { filterTarget: FilterTarget; setFilterTarget: Dispatch<SetStateAction<FilterTarget>> }) {
-  const [includeWord, setIncludeWord] = useState('');
-  const [excludeWord, setExcludeWord] = useState('');
-
-  return <div className='bg-[#272b33] p-2 text-base '></div>;
+function Button({ children }: { children: string }) {
+  return (
+    <button type='button' className='border border-[#7a828e] rounded-md bg-[#0a0c10] text-[#f0f3f6] py-1 px-2 mr-2 my-1'>
+      {children}
+    </button>
+  );
 }
