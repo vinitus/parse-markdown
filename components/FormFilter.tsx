9b22fbece0b0698b67c4c1674fd2c4d2bfd1eb67
vinitus <tlsdnrng@gmail.com>
2023-08-26 19:11:03 +0900

    input 태그와 button 태그 만듬

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -1,3 +1,22 @@
 export default function FormFilter() {
-  return <div className='bg-[#272b33] p-2 text-base'></div>;
+  return (
+    <div className='bg-[#272b33] p-2 text-base '>
+      <Input />
+      <Button>test</Button>
+      <br />
+      <Input />
+    </div>
+  );
+}
+
+function Input() {
+  return <input type='text' className='border border-[#7a828e] rounded-md py-1 px-2 bg-[#0a0c10] text-[#f0f3f6] text-xs mr-2 my-1' />;
+}
+
+function Button({ children }: { children: string }) {
+  return (
+    <button type='button' className='border border-[#7a828e] rounded-md bg-[#0a0c10] text-[#f0f3f6] py-1 px-2 mr-2 my-1'>
+      {children}
+    </button>
+  );
 }
