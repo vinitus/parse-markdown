6c148b2cc2849493787b49105666324db7b6e7fb
vinitus <tlsdnrng@gmail.com>
2023-08-26 19:46:44 +0900

    훅은 못써도, children props로 넘길수는 있다

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import { Input } from './Input';
 
 export default function FormFilter() {
   return (
     <div className='bg-[#272b33] p-2 text-base '>
-      <Input tagType='input' />
+      <Input tagType='input'>
+        <input type='text' />
+      </Input>
     </div>
   );
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,6 @@
-// import { useState } from 'react';
 import navbarCss from '@/components/navbar.module.css';
 
-export function Input({ tagType }: { tagType: 'input' | 'button' }) {
-  // const [value, setValue] = useState('검색');
-  console.log(tagType);
-
+export function Input({ tagType, children }: { tagType: 'input' | 'button'; children?: React.ReactNode }) {
   if (tagType === 'button')
     return (
       <button className={navbarCss.navbarSearchBox}>
@@ -15,7 +11,7 @@ export function Input({ tagType }: { tagType: 'input' | 'button' }) {
 
   return (
     <div className={navbarCss.navbarSearchBox}>
-      <input type='text' placeholder='검색' className={`${navbarCss.navbarSearchBox} text-[#0a0c10]`} />
+      {children}
       <p className={navbarCss.navbarSearchBoxButton}>검색</p>
     </div>
   );
