8f635782909b3252b02654dc55baec4c38016de9
vinitus <tlsdnrng@gmail.com>
2023-08-26 20:34:36 +0900

    단어 추가에 대한 처리

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -11,23 +11,46 @@ export default function FormFilter({
   const [includeWord, setIncldueWord] = useState('');
   const [excludeWord, setExcldueWord] = useState('');
 
+  function enterFn(event: React.KeyboardEvent<HTMLInputElement>, target: 'include' | 'exclude', dispatcher: React.Dispatch<React.SetStateAction<string>>) {
+    if (event.key !== 'Enter') return;
+    setFilterTarget((prev) => {
+      if (isDuplicated(prev[target], target)) return prev;
+
+      prev[target].push(includeWord);
+      return prev;
+    });
+
+    dispatcher('');
+  }
+
   return (
     <div className='bg-[#272b33] p-2 text-base'>
-      <Input placeholder='include' value={includeWord} dispatcher={setIncldueWord} />
+      <Input placeholder='include' value={includeWord} dispatcher={setIncldueWord} enterFn={enterFn} />
       <Button>추가하기</Button>
       <br />
-      <Input placeholder='exclude' value={excludeWord} dispatcher={setExcldueWord} />
+      <Input placeholder='exclude' value={excludeWord} dispatcher={setExcldueWord} enterFn={enterFn} />
       <Button>추가하기</Button>
     </div>
   );
 }
 
-function Input({ placeholder, value, dispatcher }: { placeholder: string; value: string; dispatcher: React.Dispatch<React.SetStateAction<string>> }) {
+function Input({
+  placeholder,
+  value,
+  dispatcher,
+  enterFn,
+}: {
+  placeholder: 'include' | 'exclude';
+  value: string;
+  dispatcher: React.Dispatch<React.SetStateAction<string>>;
+  enterFn: (event: React.KeyboardEvent<HTMLInputElement>, target: 'include' | 'exclude', dispatcher: React.Dispatch<React.SetStateAction<string>>) => void;
+}) {
   return (
     <input
       type='text'
       value={value}
       onChange={(event) => dispatcher(event.target.value)}
+      onKeyDown={(event) => enterFn(event, placeholder, dispatcher)}
       placeholder={placeholder}
       className='border border-[#7a828e] rounded-md py-1 px-2 bg-[#0a0c10] text-[#f0f3f6] text-xs mr-2 my-1'
     />
@@ -40,4 +63,8 @@ function Button({ children }: { children: string }) {
       {children}
     </button>
   );
+}
+
+function isDuplicated(arr: string[], str: string) {
+  return arr.includes(str);
 }
