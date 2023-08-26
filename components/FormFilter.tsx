4ef9ce05139a1ac89e18ed9ee7932991c30f8bdb
vinitus <tlsdnrng@gmail.com>
2023-08-26 19:15:48 +0900

    moudle css는 다른 컴포넌트에서도 적용된다

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -1,3 +1,9 @@
+import { Input } from './Header';
+
 export default function FormFilter() {
-  return <div className='bg-[#272b33] p-2 text-base '></div>;
+  return (
+    <div className='bg-[#272b33] p-2 text-base '>
+      <Input />
+    </div>
+  );
 }
