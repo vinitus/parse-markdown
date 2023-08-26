5eb8fa3e1d0762babb0f4eee1ff901f4c216ddde
vinitus <tlsdnrng@gmail.com>
2023-08-26 20:23:17 +0900

    input 태그별로 상태 추가하기

--- components/FormFilter.tsx
+++ components/FormFilter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FilterTarget } from '@/utils/backtickAlgorithm';
 
 export default function FormFilter({
@@ -7,19 +8,30 @@ export default function FormFilter({
   filterTarget: FilterTarget;
   setFilterTarget: React.Dispatch<React.SetStateAction<FilterTarget>>;
 }) {
+  const [includeWord, setIncldueWord] = useState('');
+  const [excludeWord, setExcldueWord] = useState('');
+
   return (
     <div className='bg-[#272b33] p-2 text-base'>
-      <Input placeholder='include' />
+      <Input placeholder='include' value={includeWord} dispatcher={setIncldueWord} />
       <Button>추가하기</Button>
       <br />
-      <Input placeholder='exclude' />
+      <Input placeholder='exclude' value={excludeWord} dispatcher={setExcldueWord} />
       <Button>추가하기</Button>
     </div>
   );
 }
 
-function Input({ placeholder }: { placeholder: string }) {
-  return <input type='text' placeholder={placeholder} className='border border-[#7a828e] rounded-md py-1 px-2 bg-[#0a0c10] text-[#f0f3f6] text-xs mr-2 my-1' />;
+function Input({ placeholder, value, dispatcher }: { placeholder: string; value: string; dispatcher: React.Dispatch<React.SetStateAction<string>> }) {
+  return (
+    <input
+      type='text'
+      value={value}
+      onChange={(event) => dispatcher(event.target.value)}
+      placeholder={placeholder}
+      className='border border-[#7a828e] rounded-md py-1 px-2 bg-[#0a0c10] text-[#f0f3f6] text-xs mr-2 my-1'
+    />
+  );
 }
 
 function Button({ children }: { children: string }) {
