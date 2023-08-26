5f96fec7ef338a9ac601a91779d598320b4c3bb1
vinitus <tlsdnrng@gmail.com>
2023-08-26 18:13:28 +0900

    Button component에 type을 통한 black, white Button 스타일 분기

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,11 @@
-export default function Button({ children, onClick }: { children: string; onClick: () => void }) {
+export default function Button({ children, onClick, type }: { children: string; onClick: () => void; type: 'black' | 'white' }) {
+  const className = {
+    black: 'rounded-md text-[#0a0c10] bg-[#f0f3f6] font-semibold px-2 py-1 min-h-8',
+    white: 'rounded-md bg-[#0a0c10] text-[#f0f3f6] font-semibold px-2 py-1 min-h-8 border border-[#f0f3f6',
+  };
+
   return (
-    <button onClick={onClick} className='rounded-md text-[#0a0c10] bg-[#f0f3f6] font-semibold px-2 py-1 min-h-8'>
+    <button onClick={onClick} className={className[type]}>
       {children}
     </button>
   );
