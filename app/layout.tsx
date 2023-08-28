49510a36c5fcbe1861d0ce3b635e640a1a437e47
vinitus <tlsdnrng@gmail.com>
2023-08-28 16:34:58 +0900

    css: main의 text class 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='kr'>
       <body className={myFont.className}>
         <Header />
-        <main className='bg-[var(--main-bg)] text-[]'>
+        <main className='bg-[var(--main-bg)] text-[var(--main-font)]'>
           <div className='max-w-screen-xl mx-auto p-10 flex flex-row w-full'>
             <Sidebar fileList={fileList} />
             {children}
