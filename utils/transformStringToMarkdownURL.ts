3ad0a3a7a33357db544d7abf83c185f16c0556ed
vinitus <tlsdnrng@gmail.com>
2023-10-23 17:17:39 +0900

    인자로 배열이 오는 경우에 대한 처리

--- utils/transformStringToMarkdownURL.ts
+++ utils/transformStringToMarkdownURL.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 
-export default function transformStringToMarkdownURL(filename: string) {
+export default function transformStringToMarkdownURL(fileWord: string | string[]) {
+  let filename;
+  if (Array.isArray(fileWord)) filename = fileWord.join('/');
+  else filename = fileWord;
+
   const filePath = path.join(process.cwd(), 'public', `${filename}.md`);
   return filePath;
 }
