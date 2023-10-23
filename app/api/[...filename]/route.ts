9ebcb38b2da49f151bb692806520ce25e3e8c07d
vinitus <tlsdnrng@gmail.com>
2023-10-23 17:16:26 +0900

    API 주소에 동적 라우팅 생성

--- app/api/[...filename]/route.ts
+++ app/api/[...filename]/route.ts
@@ -4,7 +4,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: { params: { filename: string } }) {
   const { filename } = params;
+  console.log('routeFilename', filename);
   const url = transformStringToMarkdownURL(filename);
+  console.log(url);
   const file = fs.readFileSync(url, 'utf8');
 
   return NextResponse.json({ markdown: file });
