b4572857217c7d518dee417c9d37cbe1efc8b8c8
vinitus <tlsdnrng@gmail.com>
2023-10-23 16:28:46 +0900

    중첩된 object를 풀어내기

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,12 @@
-export default function Sidebar({ fileList }: { fileList: string[] }) {
+import { FileDir } from '@/utils/getFileList';
+
+export default function Sidebar({ fileList }: { fileList: FileDir }) {
   return (
     <div className='sticky h-full flex-shrink-0 flex-col justify-between w-72'>
-      {fileList.map((item, idx) => (
-        <ul key={idx}>{item}</ul>
-      ))}
+      {Object.entries(fileList).map(([key, value], idx) => {
+        if (typeof value === 'object') return <Sidebar fileList={value}></Sidebar>;
+        else return <ul key={idx}>{key}</ul>;
+      })}
     </div>
   );
 }
