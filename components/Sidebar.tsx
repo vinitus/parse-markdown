f6dcf78db27d870d901202d1b9c2f52fdc5ef4f2
vinitus <tlsdnrng@gmail.com>
2023-10-23 17:17:04 +0900

    Link 태그 추가

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,17 @@
 import { FileDir } from '@/utils/getFileList';
+import Link from 'next/link';
 
-export default function Sidebar({ fileList }: { fileList: FileDir }) {
+export default function Sidebar({ fileList, addDir = '' }: { fileList: FileDir; addDir?: string }) {
   return (
     <div className='sticky h-full flex-shrink-0 flex-col justify-between w-72'>
       {Object.entries(fileList).map(([key, value], idx) => {
-        if (typeof value === 'object') return <Sidebar fileList={value}></Sidebar>;
-        else return <ul key={idx}>{key}</ul>;
+        if (typeof value === 'object') return <Sidebar fileList={value} addDir={addDir + '/' + key}></Sidebar>;
+        else
+          return (
+            <Link href={`/markdown/${addDir}/${key.split('.md')[0]}`}>
+              <ul key={idx}>{key}</ul>
+            </Link>
+          );
       })}
     </div>
   );
