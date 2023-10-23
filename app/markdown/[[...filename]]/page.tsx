4af13b551534d3b79ec1fdce0e94fb3d00a0925c
vinitus <tlsdnrng@gmail.com>
2023-10-23 17:26:44 +0900

    optioncal 동적 라우팅

---
+++ app/markdown/[[...filename]]/page.tsx
@@ -0,0 +1,29 @@
+import Markdown from '@/components/Markdown';
+import loadMarkdown from '@/utils/loadMarkdown';
+
+export default async function MarkdownEditor({ params }: { params: { filename: string | string[] } }) {
+  let { filename } = params;
+  let encodedPath;
+  if (Array.isArray(filename)) {
+    encodedPath = filename.join('/');
+    filename = filename[filename.length - 1];
+  }
+  console.log(filename);
+  if (filename) {
+    const decodedFilename = decodeURIComponent(encodedPath ? encodedPath : filename);
+    const markdownContent = await loadMarkdown(decodedFilename);
+    const markdownDataObj = {
+      markdownContent,
+      filename,
+    };
+
+    return <Markdown markdownDataObj={markdownDataObj} />;
+  } else {
+    const markdownDataObj = {
+      markdownContent: '',
+      filename: 'New Markdown',
+    };
+
+    return <Markdown markdownDataObj={markdownDataObj} />;
+  }
+}
